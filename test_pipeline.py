51afe2189a9507c31e876312f59b872d9caa3d1f
aalling-ne <alling.a@northeastern.edu>
2025-05-22 15:12:46 -0400

    added addition test for bad type input to function

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,7 +1,12 @@
 import pipeline as pl
+import pytest
 
 def test_zero_zero():
     assert(pl.add_then_power(0, 0) == 1)
 
 def test_two_four():
     assert(pl.add_then_power(2,4) == 1296)
+
+def test_string_input():
+    with pytest.raises(TypeError):
+        pl.add_then_power(1,"two")
