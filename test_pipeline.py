7cfcc7cd0dc48ae1cc99f8c5e3e7d249d5e8f71f
aalling-ne <alling.a@northeastern.edu>
2025-05-22 15:00:24 -0400

    fixed test, as 0^0 = 1

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,7 +1,7 @@
 import pipeline as pl
 
 def test_zero_zero():
-    assert(pl.add_then_power(0, 0) == 0)
+    assert(pl.add_then_power(0, 0) == 1)
 
 def test_two_four():
     assert(pl.add_then_power(2,4) == 1296)
