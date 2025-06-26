67a02ea203049c1064abc4093d006047e56f5628
aalling-ne <alling.a@northeastern.edu>
2025-06-26 16:22:14 -0400

    replaced flask's get_json() function with requests .json() function

--- test_app.py
+++ test_app.py
@@ -1,14 +1,5 @@
-from app import app as flask_app
-
 import pytest
 import requests
-
-#set up app
-# @pytest.fixture
-# def app():
-#     flask_app.testing = True
-#     with flask_app.test_client() as app:
-#         yield app
 
 BASE_URL = "http://flask-api:5000"
 
@@ -29,7 +20,7 @@ def test_get_single_item(example_item):
     requests.post(f"{BASE_URL}/api/items", json=example_item)
     response = requests.get(f"{BASE_URL}/api/items/{example_item['id']}")
     assert response.status_code == 200
-    assert response.get_json() == example_item
+    assert response.json() == example_item
 
 def test_update_single_item(example_item):
     requests.post(f"{BASE_URL}/api/items", json=example_item)
@@ -38,7 +29,7 @@ def test_update_single_item(example_item):
 
     response = requests.put(f"{BASE_URL}/api/items/{example_item['id']}", json = updated_item)
     assert response.status_code == 200
-    assert response.get_json() == updated_item
+    assert response.json() == updated_item
 
 def test_delete_single_item(example_item):
     requests.post(f"{BASE_URL}/api/items", json=example_item)
