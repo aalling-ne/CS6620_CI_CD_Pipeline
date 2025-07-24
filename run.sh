4596eccaa26664b114e73d8bbe4fadb37720105e
aalling-ne <alling.a@northeastern.edu>
2025-07-24 14:23:22 -0400

    fixed docker compose command issue (no hyphen)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker-compose up
+docker compose up --build
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from flask-api-test
+docker compose -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from flask-api-test
